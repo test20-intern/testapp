ce19acd82a0e223a0bdcb2856329659ba33c816f
Suwantha Darshana <Suwanthad@ceylincolife.ins>
2024-01-16 16:47:57 +0530

    policy inquiry

--- src/components/PolicyInquiry.js
+++ src/components/PolicyInquiry.js
@@ -1,33 +1,91 @@
 import React from "react";
 import ResponsiveDrawer from "./ResponsiveDrawerLeft";
-import { Box, Container, Paper, TextField } from "@mui/material";
+import { purple } from '@mui/material/colors';
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function PolicyInquiry() {
   return (
     <div>
       <ResponsiveDrawer />
-      <Box sx={{
-      height: 500,
-      width: "70%",
-      marginLeft: 35,
-      "& .actions": {
-        color: "text.secondary",
-      },
-      "& .textPrimary": {
-        color: "text.primary",
-      },
-      '@media (max-width: 600px)': {
-        width: '100%', // Set width to 100% when screen width is under 600px
-        marginLeft: 0,  // Optional: remove left margin for smaller screens
-      },
-    }}>
-      <Paper elevation={3}>
-      <Container>
-      <Box sx={{ bgcolor: '#', height: '100vh' }} />
-      <TextField fullWidth label="fullWidth" id="fullWidth" />
-      </Container>
-      </Paper>
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 1,
+            width: 1,
+            height: 500,
+            marginLeft: 35,
+            marginRight: 5,
+          },
+          "@media (max-width: 600px)": {
+            "& > :not(style)": {
+              width: "100%", // Set width to 100% for all children when screen width is under 600px
+              marginLeft: 5, // Remove left margin for smaller screens
+              marginRight: 5, // Remove right margin for smaller screens
+            },
+          },
+        }}
+      >
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography>Search Policy</Typography>
+          <TextField
+            label="Policy No"
+            fullWidth
+            margin="dense"
+            sx={{ mb: 2 }}
+          />
+
+          <TextField label="NIC No" fullWidth margin="dense" sx={{ mb: 2 }} />
+
+          <TextField label="Name" fullWidth margin="dense" sx={{ mb: 2 }} />
+
+          <TextField
+            label="Client ID"
+            fullWidth
+            margin="dense"
+            sx={{ mb: 2 }}
+          />
+
+          <TextField label="SO Code" fullWidth margin="dense" sx={{ mb: 2 }} />
+
+          <Stack spacing={10} direction="row" justifyContent="center">
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#760616",
+                "&:hover": {
+                  backgroundColor: "#760616" ,
+                },
+                width: "90px",
+                maxWidth: "100%",
+              }}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#760616",
+                "&:hover": {
+                  backgroundColor: "#760616" ,
+                },
+                width: "90px",
+                maxWidth: "100%",
+              }}
+            >
+              Search
+            </Button>
+          </Stack>
+        </Paper>
+      </Box>
     </div>
   );
 }
